312aed50f1e8a12e0f28e237e98b70d383603a04
Michael dos Santos <michael-santos@outlook.com.br>
2024-07-16 21:51:42 -0300

    Add set centralized initial view on map

--- appfinder/src/components/mapCanvas.tsx
+++ appfinder/src/components/mapCanvas.tsx
@@ -3,32 +3,38 @@
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Imovel } from '@/interfaces/Imovel';
-import MapIcon from '@mui/icons-material/Map';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import { useMemo } from 'react';
 
 export default function MapCanvas({dados} : {
   dados : Imovel[]
 }) {
-
-  
-
   const imoveisfiltered = dados.filter(_ => _.longitude != null && _.latitude !== null);
   
   const markers = useMemo(() => imoveisfiltered.map(imovel => (
     <Marker key={imovel.id}
       longitude={imovel.longitude}
       latitude={imovel.latitude}>
-      <MapIcon />
+      <FmdGoodIcon />
     </Marker>)
   ), [imoveisfiltered]);
+
+  const calculateMapCenterCoordinates = imoveisfiltered.reduce((previousValue, currentValue, currentIndex, imoveis) => {
+    previousValue[0] = parseFloat(currentValue.latitude) + previousValue[0];
+    previousValue[1] = parseFloat(currentValue.longitude) + previousValue[1];
+    return previousValue;
+  }, [0, 0])
+
+  console.log(`Num imóveis: ${imoveisfiltered.length}`)
+  console.log(`Coordenadas ${calculateMapCenterCoordinates[0]} ${calculateMapCenterCoordinates[1]}`)
 
   return (
     <Map
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       initialViewState={{
-        longitude: -47.461283,
-        latitude: -23.515821,
-        zoom: 14
+        latitude: calculateMapCenterCoordinates[0] / imoveisfiltered.length,
+        longitude: calculateMapCenterCoordinates[1] / imoveisfiltered.length,
+        zoom: 13
       }}
       style={{width: 600, height: 400}}
       mapStyle="mapbox://styles/mapbox/streets-v12"
