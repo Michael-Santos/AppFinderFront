62c5726327cff0d9273e6c196c0ee654f8b779e6
Michael dos Santos <michael-santos@outlook.com.br>
2024-10-02 16:36:21 -0300

    Add fetch properties

--- AppFinderFront/src/routes/consulta/Consulta.tsx
+++ AppFinderFront/src/routes/consulta/Consulta.tsx
@@ -1,10 +1,44 @@
-import { useParams } from "react-router-dom"
+import { Imovel } from "../../interfaces/Imovel";
+import { useEffect, useState } from "react";
 
 const Consulta = () => {
-    const { id } = useParams();    
-    
+    const [imoveis, setImoveis] = useState<Imovel[]>(); 
+
+    useEffect(() => {
+        fetch('http://localhost:8080/Property')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data);
+            setImoveis(data);
+        })
+    }, []);
+
     return (
-        <h1>Consulta {id}</h1>
+        <>
+            <table>
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {imoveis?.map((imovel : Imovel) => (
+                    <tr>
+                        <td>{imovel.id}</td>
+                        <td>{imovel.bedrooms}</td>
+                        <td>{imovel.bedrooms}</td>
+                        <td>{imovel.garage}</td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        </>
+                
     )
 }
 
