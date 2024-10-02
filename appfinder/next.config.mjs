ef24b58a0e7ca1d536df44406c4bb7c59e945a3e
Michael dos Santos <michael-santos@outlook.com.br>
2024-10-02 12:06:27 -0300

    Remove Next.js project

--- appfinder/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN_MAPBOX_TOKEN
-    }
-};
-
-export default nextConfig;
