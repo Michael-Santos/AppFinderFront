8288ca23385be228d036d8e09850e09390a2b624
Michael dos Santos <michael-santos@outlook.com.br>
2024-10-04 12:25:53 -0300

    Fix map

--- AppFinderFront/src/components/MapCanvas.tsx
+++ AppFinderFront/src/components/MapCanvas.tsx
@@ -1,45 +1,37 @@
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Imovel } from '../interfaces/Imovel';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 
-const MapCanvas = ( { imoveis } : {
-    imoveis : Imovel[]
+const MapCanvas = ( { imoveis,  coordinates} : {
+    imoveis : Imovel[], coordinates : number[]
 } ) => {
-    console.log(imoveis)
-    const imoveisfiltered = imoveis.filter(_ => _.longitude != null && _.latitude !== null);
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
 
-    const markers = useMemo(() => imoveisfiltered.map(imovel => (
+    const markers = useMemo(() => imoveis.map(imovel => (
     <Marker key={imovel.id}
         longitude={Number(imovel.longitude)}
         latitude={Number(imovel.latitude)}>
         <FmdGoodIcon />
     </Marker>)
-    ), [imoveisfiltered]);
+    ), [imoveis]);
 
-    const calculateMapCenterCoordinates = imoveisfiltered.reduce((previousValue, currentValue, currentIndex, imoveis) => {
-        console.log(`Latitude ${currentValue.latitude} Longitude: ${currentValue.longitude}`)
-        previousValue[0] = parseFloat(currentValue.latitude) + previousValue[0];
-        previousValue[1] = parseFloat(currentValue.longitude) + previousValue[1];
-    return previousValue;
-    }, [0, 0])
-
-    console.log(`Num imóveis: ${imoveisfiltered.length}`)
-    console.log(`Coordenadas ${calculateMapCenterCoordinates[0] / imoveisfiltered.length} ${calculateMapCenterCoordinates[1] / imoveisfiltered.length}`)
+    console.log(`Num imóveis: ${imoveis.length}`)
 
     return (
     <Map
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
         initialViewState={{
-        latitude: calculateMapCenterCoordinates[0] / imoveisfiltered.length,
-        longitude: calculateMapCenterCoordinates[1] / imoveisfiltered.length,
-        zoom: 13
+            latitude: coordinates[0] / imoveis.length,
+            longitude: coordinates[1]  / imoveis.length,
+            zoom: 13
         }}
         style={{width: 600, height: 400}}
         mapStyle="mapbox://styles/mapbox/streets-v12"
     >
-        {/* {markers} */}
+        {markers}
     </Map>
     )
 }
--- AppFinderFront/src/routes/mapa/Mapa.tsx
+++ AppFinderFront/src/routes/mapa/Mapa.tsx
@@ -4,8 +4,9 @@ import { useParams } from "react-router";
 import MapCanvas from "../../components/MapCanvas";
 
 const Map = () => {
-    const [imoveis, setImoveis] = useState<Imovel[]>([]); 
-    const { id }  = useParams();
+    const [imoveis, setImoveis] = useState<Imovel[]>([])
+    const { id }  = useParams<string>()
+    const [coordinates, setCoordinates] = useState<number[]>([0, 0])
     
     useEffect(() => {
         fetch(`http://localhost:8080/Query/${id}`)
@@ -13,15 +14,19 @@ const Map = () => {
             return response.json()
         })
         .then((data) => {
-            setImoveis(data);
+            const imoveisfiltered = data.filter(_ => _.longitude != null && _.latitude !== null);
+            setCoordinates(imoveisfiltered.reduce((previousValue : number[], currentValue : Imovel) => {
+                console.log(`Latitude ${currentValue.latitude} Longitude: ${currentValue.longitude}`)
+                previousValue[0] = parseFloat(currentValue.latitude) + previousValue[0]
+                previousValue[1] = parseFloat(currentValue.longitude) + previousValue[1]
+            return previousValue
+            }, [0, 0]));
+            
+            setImoveis(imoveisfiltered);
         })
     }, []);
-    
-    return (
-        <>
-            <MapCanvas imoveis={imoveis} />
-        </>
-    )
+
+    return imoveis.length > 0 && coordinates[0] != 0 && <MapCanvas imoveis={imoveis} coordinates={coordinates} />
 }
 
 export default Map
